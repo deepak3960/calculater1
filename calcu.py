8e2bebde02567bf9ecb34ed057e58a4cf3cf0d1f
deepak3960 <deepakcktd97@gmail.com>
2025-08-26 18:25:03 +0530

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a first number"))
+num2=int(input("Enter a second number"))
+add=num+num2
+sub=num-num2
+mul=num*num2
+div=num/num2
+print("Addition",add)
+print("Substraction",sub)
+print("Multiplication",mul)
+print("Division",div)
+
