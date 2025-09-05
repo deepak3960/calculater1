85684358a6ab010c33bd8867fa52e3748055d8da
deepak3960 <deepakcktd97@gmail.com>
2025-09-06 01:00:23 +0530

    Update aasignment2.py

--- aasignment2.py
+++ aasignment2.py
@@ -1,1 +1,15 @@
+#1
+'''
+num=int(input("Enter a number : "))
+if num%2==0:
+    print(num,"is an even number.")
+else:
+    print(num,"is an odd number.")
+'''
+#2
+v=0
+for i in range(51):
+    v=v+i
+print("The sum of numbers from 1 to 50 is:",v)    
+
 
