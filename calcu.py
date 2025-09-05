9406afee4acb5d1d07c58ddfc60e91a9d2be98ea
deepak3960 <deepakcktd97@gmail.com>
2025-09-06 00:30:29 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -12,5 +12,6 @@ print("Division",div)
 '''
 fname=str(input("Enter your first name : "))
 lname=str(input("Enter your last name : "))
-print("Hello ",fname," ",lname ,"! Welcome to the pyhton program")
+print("Hello",fname," ",lname,"! Welcome to the pyhton program")
+
 
