812a8a496ede9d89c0b25196615e1e7211da98ba
deepak3960 <deepakcktd97@gmail.com>
2025-09-06 00:28:08 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -1,3 +1,4 @@
+'''
 num=int(input("Enter a first number"))
 num2=int(input("Enter a second number"))
 add=num+num2
@@ -8,4 +9,8 @@ print("Addition",add)
 print("Substraction",sub)
 print("Multiplication",mul)
 print("Division",div)
+'''
+fname=str(input("Enter your first name : "))
+lname=str(input("Enter your last name : "))
+print("Hello ",fname," ",lname ,"! Welcome to the pyhton program")
 
